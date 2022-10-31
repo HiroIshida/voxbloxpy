9648e48f0137082b44fe63af49c2a0dfd666d3a1
HiroIshida <spitfire.docomo@gmail.com>
2022-10-31 23:37:02 +0900

    [modify_original] complete template specialization must be inline

--- include/voxblox/simulation/simulation_world_inl.h
+++ include/voxblox/simulation/simulation_world_inl.h
@@ -70,7 +70,7 @@ void SimulationWorld::generateSdfFromWorld(FloatingPoint max_dist,
 }
 
 template <>
-void SimulationWorld::setVoxel(FloatingPoint dist, const Color& color,
+inline void SimulationWorld::setVoxel(FloatingPoint dist, const Color& color,
                                TsdfVoxel* voxel) const {
   voxel->distance = static_cast<float>(dist);
   voxel->color = color;
@@ -79,7 +79,7 @@ void SimulationWorld::setVoxel(FloatingPoint dist, const Color& color,
 
 // Color ignored.
 template <>
-void SimulationWorld::setVoxel(FloatingPoint dist, const Color& /*color*/,
+inline void SimulationWorld::setVoxel(FloatingPoint dist, const Color& /*color*/,
                                EsdfVoxel* voxel) const {
   voxel->distance = static_cast<float>(dist);
   voxel->observed = true;
