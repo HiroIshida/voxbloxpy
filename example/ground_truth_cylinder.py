ebefe3e0134d00b69f782c526e697d93de3db0fd
HiroIshida <spitfire.docomo@gmail.com>
2022-11-01 06:31:35 +0900

    Update example

--- example/ground_truth_cylinder.py
+++ example/ground_truth_cylinder.py
@@ -1,7 +1,6 @@
 import numpy as np
 import time
 from _voxbloxpy import get_test_esdf
-import plotly
 import plotly.graph_objects as go
 
 esdf = get_test_esdf()
@@ -11,10 +10,10 @@ xlin = np.linspace(-3.0, 3.0, 100)
 ylin = np.linspace(-3.0, 3.0, 100)
 zlin = np.linspace(-2.0, 4.0, 100)
 X, Y, Z = np.meshgrid(xlin, ylin, zlin)
-pts_local = np.array(list(zip(X.flatten(), Y.flatten(), Z.flatten())))
+pts_global = np.array(list(zip(X.flatten(), Y.flatten(), Z.flatten())))
 
 ts = time.time()
-values = esdf.get_sd_batch(pts_local, 100.0)
+values = esdf.get_sd_batch(pts_global, 100.0)
 print(values)
 
 
--- example/incremetal_update.py
+++ example/incremetal_update.py
@@ -1,26 +1,54 @@
 import numpy as np
-from _voxbloxpy import EsdfMap
+from _voxbloxpy import EsdfMap, TsdfMap
+import plotly.graph_objects as go
 
 
-quat_wxyz = [0.0, 1.0, 0.0, 0.0]
-pos = [-1.0, 0.0, 0.0]
-camera_pose = pos + quat_wxyz
+def create_esdf():
+    quat_wxyz = [0.0, 1.0, 0.0, 0.0]
+    pos = [-1.0, 0.0, 0.0]
+    camera_pose = pos + quat_wxyz
 
-r_sphere = 0.5
+    r_sphere = 0.5
+    ylin = np.linspace(-1.0, 1.0, 100)
+    zlin = np.linspace(-1.0, 1.0, 100)
+    y_grid, z_grid = np.meshgrid(ylin, zlin)
+    pts_yz_plane = np.array(list(zip(y_grid.flatten(), z_grid.flatten())))
+    inside_circle = np.sum(pts_yz_plane ** 2, axis=1) < r_sphere - 0.02
+    pts_yz_plane_inner = pts_yz_plane[inside_circle]
+    pts_x = -np.sqrt(1.0 - np.sum(pts_yz_plane_inner ** 2, axis=1))
+
+    pts = np.zeros((len(pts_x), 3))
+    pts[:, 0] = pts_x
+    pts[:, 1:] = pts_yz_plane_inner
+
+    esdfmap = EsdfMap(0.01, 16)
+    esdfmap.update(camera_pose, pts, False)
+    return esdfmap
+
+esdf = create_esdf()
+
+xlin = np.linspace(-1.0, 1.0, 100)
 ylin = np.linspace(-1.0, 1.0, 100)
 zlin = np.linspace(-1.0, 1.0, 100)
-y_grid, z_grid = np.meshgrid(ylin, zlin)
-pts_yz_plane = np.array(list(zip(y_grid.flatten(), z_grid.flatten())))
-inside_circle = np.sum(pts_yz_plane ** 2, axis=1) < r_sphere - 0.02
-pts_yz_plane_inner = pts_yz_plane[inside_circle]
-pts_x = -np.sqrt(1.0 - np.sum(pts_yz_plane_inner ** 2, axis=1))
+X, Y, Z = np.meshgrid(xlin, ylin, zlin)
+pts = np.array(list(zip(X.flatten(), Y.flatten(), Z.flatten())))
+values = esdf.get_sd_batch(pts, 100.0)
+print(values)
 
-pts = np.zeros((len(pts_x), 3))
-pts[:, 0] = pts_x
-pts[:, 1:] = pts_yz_plane_inner
-
-esdf = EsdfMap(0.01, 16)
-esdf.update(camera_pose, pts)
+#fig = go.Figure(data=go.Isosurface(
+#    x=X.flatten(),
+#    y=Y.flatten(),
+#    z=Z.flatten(),
+#    value=values,
+#    isomin=-1.0,
+#    isomax=2.0,
+#    opacity=0.1,
+#    surface_count=10,
+#    colorbar_nticks=5,
+#    colorscale='Plotly3',
+#    caps=dict(x_show=False, y_show=False)
+#    ))
+#fig.show()
 
 #import matplotlib.pyplot as plt
 #fig = plt.figure()
