951f827b7759b5a6308dc94c71418faf8515e745
HiroIshida <spitfire.docomo@gmail.com>
2022-11-03 07:52:00 +0900

    Update ros node demo

---
+++ example/ros/download_rosbag.sh
@@ -0,0 +1,1 @@
+gdown https://drive.google.com/uc?id=13s3Kg3oHVV6htc7s9uIv9h7NVqgnQctg
--- example/ros/example_node.py
+++ example/ros/example_node.py
@@ -23,22 +23,33 @@ grid_sdf_list: List[GridSDF] = []
 
 
 def hook(esdf_map: EsdfMap):
+    # this function might be too heavy.
+    # I just do the following to make an animation. but do not do that
+    # in the realtime application!
+    ts = time.time()
     info = esdf_map.get_voxel_info()
     measure_grid = info.get_boundary_grid(grid_size=0.1)
     grid_sdf = esdf_map.get_grid_sdf(measure_grid)
     grid_sdf_list.append(grid_sdf)
+    te = time.time()
+    rospy.loginfo("elapsed time for getting gridsdf {} sec".format(te - ts))
 
 
 node = EsdfNode(config, hook=hook)
 
-time.sleep(5)
+time.sleep(20)  # stop node after a while
 node.callback_running = False
 
-# save figures
+# save figures as png
 fig_path = Path(__file__).resolve().parent / "figs"
 fig_path.mkdir(exist_ok=True)
 for i, grid_sdf in enumerate(grid_sdf_list):
-    fig = grid_sdf.render_volume(isomin=-0.2, isomax=0.3, show=False)
-    name = fig_path / "sdfplot-{}.png".format(i)
+    fig = grid_sdf.render_volume(isomin=-0.2, isomax=2.0, show=False)
+    name = fig_path / "sdfplot-{:03d}.png".format(i)
     rospy.loginfo("writing figure to {}".format(name))
     fig.write_image(name)
+
+# save final figure as html
+file_name = fig_path / "final_sdf.html"
+fig = grid_sdf_list[-1].render_volume(isomin=-0.2, isomax=2.0, show=False)
+fig.write_html(file_name)
